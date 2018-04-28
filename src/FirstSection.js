7f2a5e64dbaeba47b3b013be52fc4fcd045465c1
Mohamed Elkarim <mohdawad2010@hotmail.com>
2018-04-28 13:24:54 -0400

    created left and right components

--- src/FirstSection.js
+++ src/FirstSection.js
@@ -7,11 +7,11 @@ class FirstSection extends Component {
   render() {
     return (
       <div className="FirstSection">
-        <div className="container">
+        {/* <div className="container">
           <h1>
             Are you like anyother small business owner frustrated with invoices coming in daily and loosing track of them? Here we have an effective and easy solution for you!! Follow along!
           </h1>
-        </div>
+        </div> */}
       </div>
     );
   }
--- src/Home.js
+++ src/Home.js
@@ -24,7 +24,7 @@ const HomepageHeading = ({ mobile }) => (
   <Container text>
     <Header
       as='h1'
-      content='Imagine-a-Company'
+      content='Hemzy'
       inverted
       style={{
         fontSize: mobile ? '2em' : '4em',
@@ -43,9 +43,14 @@ const HomepageHeading = ({ mobile }) => (
         marginTop: mobile ? '0.5em' : '1.5em',
       }}
     />
-    <Button primary size='huge'>
-      Get Started
-      <Icon name='right arrow' />
+    <Button white size='huge'>
+      <div>
+  <Image src='slackIcon.jpg' avatar />
+  <span>Add to <span>Slack</span></span>
+</div>
+)
+
+      {/* <Icon name='right arrow' /> */}
     </Button>
   </Container>
 )
@@ -85,8 +90,9 @@ class DesktopContainer extends Component {
                 <Menu.Item as='a'>Company</Menu.Item>
                 <Menu.Item as='a'>Careers</Menu.Item>
                 <Menu.Item position='right'>
-                  <Button as='a' inverted={!fixed}>Log in</Button>
-                  <Button as='a' inverted={!fixed} primary={fixed} style={{ marginLeft: '0.5em' }}>Sign Up</Button>
+                  {/* User Authentication if needed. */}
+                  {/* <Button as='a' inverted={!fixed}>Log in</Button>
+                  <Button as='a' inverted={!fixed} primary={fixed} style={{ marginLeft: '0.5em' }}>Sign Up</Button> */}
                 </Menu.Item>
               </Container>
             </Menu>

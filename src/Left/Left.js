c0da1e8f1fc6acf4f0b627021db02292453e42a3
Mike Nabil <maikel_nabil@yahoo.com>
2018-04-28 18:47:36 -0400

    removed unused code

--- src/Left/Left.js
+++ src/Left/Left.js
@@ -3,35 +3,19 @@ import './Left.css'
 import hermes from './hermes.jpg'
 import slack from './slackIcon.jpg'
 
-import {
-  Button,
-  Container,
-  Divider,
-  Grid,
-  Header,
-  Icon,
-  Image,
-  List,
-  Menu,
-  Responsive,
-  Segment,
-  Sidebar,
-  Visibility
-} from 'semantic-ui-react'
-
 class Left extends Component {
   render () {
     return (
       <div className='left'>
         <header className='h1'>
-          <img src={hermes} />
+          <img src={hermes} alt='logo' />
           Hermyz
         </header>
         <div className='slack'>
           <h4>
             Install on Slack
           </h4>
-          <button><img src={slack} />Add to Slack</button>
+          <button><img src={slack} alt='slack' />Add to Slack</button>
         </div>
       </div>
     )

e7f9a96dad84bf95bebcbab71dcd04cd5c066ff3
Mohamed Elkarim <mohdawad2010@hotmail.com>
2018-04-28 17:20:02 -0400

    finished preparing left seciton

--- src/Left/Left.js
+++ src/Left/Left.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './Left.css'
 import hermes from './hermes.jpg'
+import slack from './slackIcon.jpg'
 
 import {
   Button,
@@ -22,13 +23,24 @@ class Left extends Component {
   render () {
     return (
       <div className='left'>
-        <Header as='h1' floated='left'>
+        <header className="h1">
+          <img src={hermes}></img>
+          Hermyz
+        </header>
+        <div className="slack">
+          <h4>
+            Install on Slack
+          </h4>
+          {/* <hr></hr> */}
+          <button><img src={slack}></img>Add to Slack</button>
+        </div>
+        {/* <Header className='brand' as='h2'>
           <Image circular src={hermes} />
         Hemzy
-        </Header>
-        <Container textAlign='center'>
+        </Header> */}
+        {/* <Container textAlign='center'>
           Connect to our Slack plug-in service!
-        </Container>
+        </Container> */}
       </div>
     )
   }

24f5dc09aeed03db0cf9935be6ca1cac38724f6e
Mohamed Elkarim <mohdawad2010@hotmail.com>
2018-04-28 13:03:42 -0400

    added sementic

--- src/Home.js
+++ src/Home.js
@@ -14,7 +14,7 @@ import {
   Segment,
   Sidebar,
   Visibility,
-} from '../semantic/dist/semantic.min.css'
+} from 'semantic-ui-react'
 
 /* eslint-disable react/no-multi-comp */
 /* Heads up! HomepageHeading uses inline styling, however it's not the best practice. Use CSS or styled components for

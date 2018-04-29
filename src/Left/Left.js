a6422b5917c67797514aa0f50c5a0589161a432d
Mike Nabil <maikel_nabil@yahoo.com>
2018-04-28 22:04:34 -0400

    various updates

--- src/Left/Left.js
+++ src/Left/Left.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './Left.css'
-import hermes from './hermes.jpg'
+import hermyz from './favicon.png'
 import slack from './slackIcon.jpg'
 
 class Left extends Component {
@@ -8,7 +8,7 @@ class Left extends Component {
     return (
       <div className='left'>
         <header className='h1'>
-          <img src={hermes} alt='logo' />
+          <img src={hermyz} alt='logo' />
           Hermyz
         </header>
         <div className='slack'>
